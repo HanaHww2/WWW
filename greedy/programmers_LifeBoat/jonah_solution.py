e913e45637fe4b07a9f17174c83f7d67b59eeef0
Terri <terricodes@gmail.com>
2022-04-18 21:10:15 +0900

    Create jonah_solution.py
    오늘은 main으로 바로 푸시합니다...!

---
+++ greedy/programmers_LifeBoat/jonah_solution.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(people, limit):
+
+    boat = 0
+    people.sort()
+
+    q = deque(people)
+    w = 0
+    cnt = 0
+    while q:
+        if len(q) >= 2:
+            if q[0] + q[-1] <= limit:
+                q.pop()
+                q.popleft()
+                boat += 1
+            elif q[0] + q[-1] > limit:
+                q.pop()
+                boat += 1
+        else:
+            if q[0] <= limit:
+                q.pop()
+                boat += 1
+    return boat
