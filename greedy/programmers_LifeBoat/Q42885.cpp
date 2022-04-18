df9e970d7efd5f4c0b86bc89ae2c3e07fc92a602
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2022-04-18 20:46:03 +0900

    개인 풀이 추가 #1

---
+++ greedy/programmers_LifeBoat/Q42885.cpp
@@ -0,0 +1,23 @@
+﻿// 탐욕법_구명보트: https://programmers.co.kr/learn/courses/30/lessons/42885
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0, head = 0, tail = people.size() - 1;
+    sort(people.begin(), people.end());
+
+    while (head <= tail)
+    {
+        if (people[head] + people[tail] <= limit)
+        {
+            head++; tail--;
+        }
+        else
+            tail--;
+
+        answer++;
+    }
+
+    return answer;
+}
