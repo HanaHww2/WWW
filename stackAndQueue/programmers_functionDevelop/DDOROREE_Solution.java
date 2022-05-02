4df050c55ea2b597820d2362d0b64f97b76885a9
YunDaHyee <yon914@naver.com>
2022-05-02 20:25:57 +0900

    fix: DDOROREE's Solution

--- stackAndQueue/programmers_functionDevelop/DDOROREE_Solution.java
+++ stackAndQueue/programmers_functionDevelop/DDOROREE_Solution.java
@@ -5,23 +5,22 @@ import java.util.List;
 import java.util.Queue;
 
 class DDOROREE_Solution {
-    public int solution(String numbers) {
-    	 public Integer[] solution(int[] progresses, int[] speeds) {
-    	        List<Integer>	days = new ArrayList<Integer>();
-    			Queue<Integer> queue = new LinkedList<Integer>();
-    			
-    			for( int i=0;i<speeds.length;i++ ) {
-    				int currentDeployDate = (int)Math.ceil((100-progresses[i])/(double) speeds[i]); // 완료일 계산하는 거
-    				if( !queue.isEmpty() && queue.peek()<currentDeployDate ){
-    					days.add( queue.size() );
-    					queue.clear();
-    				}
-    				queue.offer( currentDeployDate );
-    			}
-    			
-    			days.add( queue.size() );
-    			
-    			Integer[] result = days.toArray( new Integer[]{days.size()} ); // 리스트 -> 배열
-    	        return result;
+	public Integer[] solution(int[] progresses, int[] speeds) {
+		List<Integer> days = new ArrayList<Integer>();
+		Queue<Integer> queue = new LinkedList<Integer>();
+
+		for (int i = 0; i < speeds.length; i++) {
+			int currentDeployDate = (int)Math.ceil((100 - progresses[i]) / (double)speeds[i]); // 완료일 계산하는 거
+			if (!queue.isEmpty() && queue.peek() < currentDeployDate) {
+				days.add(queue.size());
+				queue.clear();
+			}
+			queue.offer(currentDeployDate);
+		}
+
+		days.add(queue.size());
+
+		Integer[] result = days.toArray(new Integer[] {days.size()}); // 리스트 -> 배열
+		return result;
 	}
 }
