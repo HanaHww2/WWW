84232cf73537fdace1fc403d0f05359739133bd5
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2022-05-15 19:55:29 +0900

    add Ashe's Solution #15

---
+++ sort/programmers_H-Index/Ashe_Solution.cpp
@@ -0,0 +1,19 @@
+﻿// H-Index: https://programmers.co.kr/learn/courses/30/lessons/42747
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0, h, idx;
+    sort(citations.begin(), citations.end());
+
+    int max = citations[citations.size() - 1] / 2;
+    for (h = 0; h < max; h++)
+    {
+        idx = 0;
+        for (int x : citations) if (x >= h) idx++;
+        if (idx >= h) answer = h;
+    }
+
+    return answer;
+}
