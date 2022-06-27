c27f73c892e9b516b88ba57745ef24f7dd6fd71d
HanaHww2 <hanahww2@gmail.com>
2022-06-27 23:28:04 +0900

    refactor: modify hanah's solution #21

--- programmers/programmers_menuRenewal/hanah_solution.py
+++ programmers/programmers_menuRenewal/hanah_solution.py
@@ -1,3 +1,8 @@
+# 집합을 활용해보고 싶어서 도전 중
+# 테스트 통과를 못하고 있다... 수정이 필요함
+from collections import Counter, defaultdict
+
+
 def solution(orders, course):
     answer = []
     # 내림차순 정렬
@@ -16,3 +21,45 @@ def solution(orders, course):
                 comb_set.add(''.join(sorted(temp)))
 
     return sorted(list(comb_set))
+
+
+# 최초 풀이
+# dfs를 이용해서 combination 생성
+
+
+def solution(orders, course):
+    answer = []
+    comb_dict = {n: defaultdict(int) for n in course}
+
+    def mk_comb(prefix, remainder):
+        length = len(prefix)
+        if length in course:
+            print(sorted(prefix))
+            prefix = ''.join(sorted(prefix))
+            comb_dict[length][prefix] += 1
+
+        if not remainder:
+            return
+        for i, one in enumerate(remainder):
+            mk_comb(prefix+one, remainder[i+1:])
+
+    s_orders = sorted(orders, key=len)  # 길이함수를 이용해 정렬
+
+    for order in s_orders:
+        # 조합 만들기
+        mk_comb('', order)
+    print(comb_dict)
+
+    for n in course:
+        temp = []
+        prev = 2
+        for key in comb_dict[n].keys():
+            val = comb_dict[n][key]
+            if val > prev:
+                prev = val
+                temp = [key]
+            elif val == prev:
+                temp.append(key)
+        answer += temp
+
+    return sorted(answer)
