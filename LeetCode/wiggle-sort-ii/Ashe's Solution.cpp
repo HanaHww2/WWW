c1f2c65df4f8cf35afc5ce0d08548f9040c25628
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2022-07-18 18:50:58 +0900

    feat: add Ashe's Solution #29

---
+++ LeetCode/wiggle-sort-ii/Ashe's Solution.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void wiggleSort(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+
+    vector<int>::iterator p;
+    p = nums.size() % 2 == 0 ? nums.begin() + nums.size() / 2 : nums.begin() + nums.size() / 2 + 1;
+    sort(nums.begin(), p, greater<int>());
+
+    int i = 1;
+    while (i <= nums.size())
+    {
+        nums.insert(nums.begin() + i, nums[nums.size() - 1]);
+        nums.pop_back();
+        i += 2;
+    }
+}
+
+void main()
+{
+    vector<int> nums = {1,4,3,4,1,2,1,3,1,3,2,3,3 };
+    wiggleSort(nums);
+
+    for (int i : nums) cout << i << " ";
+}
