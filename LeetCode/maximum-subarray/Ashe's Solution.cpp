e54873b983c85e5c13eaf850ff7a37d4055e3905
victoryAshe <80302657+victoryAshe@users.noreply.github.com>
2022-08-01 22:01:41 +0900

    Feat: Add Ashe's Solution #33

---
+++ LeetCode/maximum-subarray/Ashe's Solution.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+
+    int great = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        nums[i] = max(nums[i], nums[i - 1] + nums[i]);
+        great = max(great, nums[i]);
+    }
+
+    return great;
+}
