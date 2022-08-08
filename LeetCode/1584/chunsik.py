fb6a0d5686bba1663851a88c56d2539a9d54c7a1
Jioo Jung <jioome17@gmail.com>
2022-08-08 22:02:13 +0900

    Create chunsik.py

---
+++ LeetCode/1584/chunsik.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        dic = {(x,y):float('inf') if i else 0 for i,(x,y) in enumerate(points)}
+        # print(dic)
+        result = 0 
+        while dic :
+            x,y = min(dic,key = dic.get) 
+            # d의 value 값중에 작은 값 가진 키 찾기
+            # min(dic.keys(), key=lambda x : dic[x]) == min(dic,key = dic.get) 
+            result += dic.pop((x,y))
+            for a,b in dic : 
+                dic[(a,b)] = min(dic[(a,b)],abs(a-x) + abs(b-y))
+            # print(dic)
+        return result
