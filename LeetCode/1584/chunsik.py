c2d624953edaa38630be307459ae607058b88ba2
Jioo Jung <jioome17@gmail.com>
2022-08-08 22:04:52 +0900

    Update chunsik.py

--- LeetCode/1584/chunsik.py
+++ LeetCode/1584/chunsik.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # 프림
+        # 프림알고리즘
         dic = {(x,y):float('inf') if i else 0 for i,(x,y) in enumerate(points)}
         # print(dic)
         result = 0 
